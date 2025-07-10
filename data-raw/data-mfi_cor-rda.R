3751f27a03667916b20688101a1ad94a97a866a5
sedzinfo <sedzinfo@gmail.com>
2025-07-10 16:53:13 +0300

    data

--- data-raw/data-mfi_cor-rda.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `data/mfi_cor.rda` dataset goes here
-
-usethis::use_data(data/mfi_cor.rda, overwrite = TRUE)
--- data-raw/data-mfi_population-rda.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `data/mfi_population.rda` dataset goes here
-
-usethis::use_data(data/mfi_population.rda, overwrite = TRUE)
